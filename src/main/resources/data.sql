02b0cc958714fa1094464e848299816cda1c2cd8
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2022-10-30 08:27:14 -0700

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username,password , enabled) values ('user','user' ,  true);
+INSERT INTO users (username,password , enabled) values ('admin','admin' , true );
+
+
+INSERT INTO authorities (username , authority ) values ('user','ROLE_user');
+INSERT INTO authorities (username , authority ) values ('admin','ROLE_admin');
